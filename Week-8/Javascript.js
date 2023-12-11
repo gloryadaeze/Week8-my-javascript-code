b2f8baa44ed952a4bfe17556b0320d909ea5acd8
gloryadaeze <daeze7@gmail.com>
2023-12-11 10:44:44 +0100

    Convert mins to sec

---
+++ Week-8/Javascript.js
@@ -0,0 +1,11 @@
+function minsToSec(m) {
+    let m = minutes;
+    const seconds = minutes * 60;
+    return seconds;
+  }
+  
+  console.log(`${minsToSec(5)} minutes is equal to ${seconds} seconds.`);
+  // Example usage:
+  //const minutes = 5;
+  //const result = minutesToSeconds(minutes);
+  //console.log(`${minutes} minutes is equal to ${result} seconds.
